b753684be8adf8cb28b3a6105f47747da004b5dc
Nikko B <ncbadoles@gmail.com>
2020-07-29 18:27:25 +0000

    updated that the snake takes in a user name

--- client.js
+++ client.js
@@ -6,12 +6,21 @@ const connect = () => {
       port: 50541
     });
     // interpret incoming data as text
-    client.setEncoding('utf8'); 
+    client.setEncoding('utf8');
+    
+    
     
     // event handle for receiving incoming message from server
-    client.on('data', function (message) {
+    client.on('data', (message) => {
       console.log(message);
     });
+
+    // Events: connect, message, error, end
+    client.on('connect', function () {
+        console.log('Succesfully connected to server');
+
+        client.write("Name: NCB");
+  });
     return client;
   }
 
